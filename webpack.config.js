2822cacfc838d71bc49db41d776c158bc8cb70ed
root <root@DESKTOP-2S2UDLA.localdomain>
2021-05-31 12:55:06 +0100

    add header section for app

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };

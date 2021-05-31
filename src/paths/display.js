4f89e2e28a4fed5386c29045d10dbd3c62b81d9d
oluwadare <wintan1418@gmail.com>
2021-05-31 14:05:51 +0100

    finish function for display.js

--- src/paths/display.js
+++ src/paths/display.js
@@ -1,17 +1,24 @@
 const view = (() => {
-function setsearchOutput(weatherData) {
-  if(!weatherData) return;
+  function setSearchOutput(weatherData) {
+    if (!weatherData) return;
 
-}
+    const setSearchOutput = document.getElementById("SearchOutput");
+    setSearchOutput.classList.add("active");
 
-constsearchOutput = document.getElementById("searchOutput");
-searchOutput.classList.add('active');
+    const city = document.getElementById("city");
+    const temperature = document.getElementById("temperature");
+    const possibility = document.getElementById("possibility");
+    const humidity = document.getElementById("humidity");
+    const wind = document.getElementById("wind");
 
-const city = document.getElementById("city");
-const temperature = document.getElementById("temperature");
-const wind = document.getElementById("wind");
-const possibility = document.getElementById("possibility");
-const humidity = document.getElementById("humidity");
+    cityName.textContent = `${weatherData.cityName}`;
+    temperature.textContent = `${weatherData.temperature} °C`;
+    possibility.textContent = `Fells like: ${weatherData.possibility} °C`;
+    humidity.textContent = `Humidity: ${weatherData.humidity} %`;
+    wind.textContent = `Wind: ${weatherData.windSpeed} km/h`;
+  }
 
-city.textContent = `${weatherData.city}`;
-temperature.textContent = `${weatherData.temperature}`;
+  return { setSearchOutput };
+})();
+
+export default view;
